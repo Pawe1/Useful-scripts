441f2d57530d6f8511596afdd8aab132be307244
PC <>
2017-04-19 11:27:56 +0200

    A little refactoring Script now works with Python 3

--- Network/WOL.py
+++ Network/WOL.py
@@ -1,7 +1,5 @@
 #! /usr/bin/python
 
-# wol
-# 
 # Wake on LAN
 #
 # Use:
@@ -9,14 +7,14 @@
 # wol computer1 computer2
 # wol 00:11:22:33:44:55
 #
-# Author: San Bergmans
-# www.sbprojects.net
-#
+# Based on scripy written by San Bergmans (www.sbprojects.net)
 
-import sys, struct, socket
+import sys
+import struct
+import socket
 
 # Configuration variables
-broadcast = ['192.168.1.255', '192.168.0.255']
+broadcast = ['192.168.0.255', '192.168.1.255']
 wol_port = 9
 
 known_computers = {
@@ -31,33 +29,30 @@ def WakeOnLan(ethernet_address):
     # Construct 6 byte hardware address
     add_oct = ethernet_address.split(':')
     if len(add_oct) != 6:
-        print "\n*** Illegal MAC address\n"
-        print "MAC should be written as 00:11:22:33:44:55\n"
+        print("*** \"" + ethernet_address + "\" is not a valid MAC address\n", end='', flush=True)
+        print("MAC should be written as 00:11:22:33:44:55\n", end='', flush=True)
         return
-    hwa = struct.pack('BBBBBB', int(add_oct[0],16),
-        int(add_oct[1],16),
-        int(add_oct[2],16),
-        int(add_oct[3],16),
-        int(add_oct[4],16),
-        int(add_oct[5],16))
+    hwa = struct.pack('BBBBBB', int(add_oct[0], 16),
+        int(add_oct[1], 16),
+        int(add_oct[2], 16),
+        int(add_oct[3], 16),
+        int(add_oct[4], 16),
+        int(add_oct[5], 16))
 
-    # Build magic packet
+    magic_packet = b'\xff' * 6 + hwa * 16
 
-    msg = '\xff' * 6 + hwa * 16
-
-# Send packet to broadcast address using UDP port 9
-
+    # Send packet to broadcast address using UDP port 9
     soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     soc.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST,1)
     for i in broadcast:
-        soc.sendto(msg,(i,wol_port))
+        soc.sendto(magic_packet, (i, wol_port))
     soc.close()
 
 if len(sys.argv) == 1:
-    print "\n*** No computer given to power up\n"
-    print "Use: 'wol computername' or 'wol 00:11:22:33:44:55'"
+    print("*** No computer given to power up\n", end='', flush=True)
+    print("Use: 'wol computername' or 'wol 00:11:22:33:44:55'")
 else:
-    for i in sys.argv:
+    for i in sys.argv[1:]:
         if i[0] != '/':
             if ":" in i:
                 # Wake up using MAC address
@@ -67,11 +62,10 @@ else:
                 if i in known_computers:
                     WakeOnLan(known_computers[i])
                 else:
-                    print "\n*** Unknown computer " + i + "\n"
-                    quit()
+                    print("*** Unknown computer \"" + i + "\"\n", end='', flush=True)
 
     if len(sys.argv) == 2:
-        print "\nDone! The computer should be up and running in a short while."
+        print("\nDone! The computer should be up and running in a short while.")
     else:
-        print "\nDone! The computers should be up and running in a short while."
+        print("\nDone! The computers should be up and running in a short while.")
         print
